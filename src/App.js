5d45ff9789d1f7eb0544e3a4b0706f7d5b571f49
kaylonm17 <you@example.com>
2023-08-14 20:52:41 -0500

    production push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar'
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
+      <Navbar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GET LIT
         </a>
       </header>
     </div>
