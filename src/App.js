ed93f2324db82f86b1eb398773a9bab5bec7e2e6
kaylonm17 <you@example.com>
2023-09-04 22:31:26 -0500

    stache

--- src/App.js
+++ src/App.js
@@ -1,51 +1,49 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Import Switch correctly
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Card from './cards';
 import WelcomeBanner from './WelcomeBanner';
 import Footer from './footer';
 
-
 function App() {
   return (
-    <Router>
-      <div className="App">
+    <div className="App">
+      <Router> {/* This should be your top-level Router */}
         <Navbar />
-          <Route exact path='/'>
-          <header className="App-header">
-          <div className="app">
-            <WelcomeBanner />
-          </div>
-        </header>  
+        <Routes>
+          <Route exact path="/">
+            <header className="App-header">
+              <div className="app">
+                <WelcomeBanner />
+              </div>
+            </header>
           </Route>
-
-          <div className="app-container">
-            <Card
-              imageSrc="path-to-your-image1.jpg"
-              title="Air Conditioning Services"
-              description="We offer top-notch AC repair and installation services."
-              imageOnRight={false}
-            />
-            <Card
-              imageSrc="path-to-your-image2.jpg"
-              title="Heating Solutions"
-              description="Stay warm with our reliable heating solutions."
-              imageOnRight={true}
-            />
-            <Card
-              imageSrc="path-to-your-image2.jpg"
-              title="Heating Solutions"
-              description="Stay warm with our reliable heating solutions."
-              imageOnRight={false}
-            />
-            {/* Add more alternating cards as needed */}
-          </div>
-
-        <Footer></Footer>
-      </div>
-    </Router>
+          <Route exact path="/air-conditioning">
+            <div className="app-container">
+              <Card
+                imageSrc="path-to-your-image1.jpg"
+                title="Air Conditioning Services"
+                description="We offer top-notch AC repair and installation services."
+                imageOnRight={false}
+              />
+            </div>
+          </Route>
+          <Route exact path="/heating-solutions">
+            <div className="app-container">
+              <Card
+                imageSrc="path-to-your-image2.jpg"
+                title="Heating Solutions"
+                description="Stay warm with our reliable heating solutions."
+                imageOnRight={true}
+              />
+            </div>
+          </Route>
+          {/* Add more routes for other services */}
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
